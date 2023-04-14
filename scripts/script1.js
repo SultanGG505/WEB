181e5117996389146bccff2ecfc8b9edefd4b0ed
Sultan <sultangordovg@gmail.com>
2023-04-14 13:25:10 +0300

    task 3 JS

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,32 +1,34 @@
-function add_zero(value){ 
-  if (value.toString().length==1) return "0"+value.toString();
-  else return value.toString();
-}
-const button_first=document.getElementById("btn_1");
-button_first.addEventListener("click",(event) => {
-    let now = new Date()
-    const day=now.getDay()
-    const year=now.getFullYear()
-    const month=now.getMonth()
-    let hours = now.getHours()
-    let minutes = now.getMinutes()
-    const seconds = now.getSeconds()
-    const month_ar=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
-    const weakd_ar=["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"];
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    const div=document.getElementsByClassName("date")[0]
-    const p = div.querySelector("p");
-    p.textContent = "Время: "+add_zero(hours)+"-"+add_zero(minutes)+"-"+add_zero(seconds)+ ampm +'\n'+"Дата: "+year+" "+ day+" "+month_ar[month]+", "+weakd_ar[day-1]
-    
-    if (div.style.display != "table"){
-      div.style.display = "table";
-    }
-    else {
-      div.style.display = "none";
-    }
-  });
-  
+(function () {
+    const btn = document.getElementById("btn_1");
+    let div = document.getElementsByClassName("date")[0]
 
+    function showDateTime() {
+        let now = new Date();
+
+        let year = now.getFullYear();
+        let month = (now.getMonth() + 1).toString().padStart(2, '0');
+        let day = now.getDate().toString().padStart(2, '0');
+        let daysOfWeek = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+        let day_week = daysOfWeek[now.getDay()];
+        let status_date = `${day_week}, ${day}-${month}-${year}`;
+
+        let hours = now.getHours().toString().padStart(2, '0');
+        let minutes = now.getMinutes().toString().padStart(2, '0');
+        let seconds = now.getSeconds().toString().padStart(2, '0');
+        let status_time = `${hours}:${minutes}:${seconds}`;
+
+        const p = div.querySelector("p");
+        p.textContent = status_date + ' ' + status_time;
+        setInterval(showDateTime, 1000);
+    }
+
+    btn.addEventListener("click", (event) => {
+        showDateTime()
+
+        if (div.style.display === "none") {
+            div.style.display = "block";
+        } else {
+            div.style.display = "none";
+        }
+    });
+}());
---
+++ scripts/script3.js
@@ -0,0 +1,18 @@
+(function () {
+    const div = document.getElementById("task3");
+    const btn = document.getElementById("btn_3");
+
+    const root = document.documentElement;
+    const all_lists = root.querySelectorAll("list");
+    let p = document.querySelectorAll("p")[1];
+
+    p.textContent = "Количество листов: " + all_lists.length
+
+    btn.addEventListener("click", (event) => {
+        if (div.style.display === "block") {
+            div.style.display = "none";
+        } else {
+            div.style.display = "block";
+        }
+    });
+}());
